51e5769740322f0365224cfad04037daebc6af64
lukemiller04 <luke.miller@modot.mo.gov>
2015-02-19 16:01:56 -0600

    Update app_3.js

--- js/app_3.js
+++ js/app_3.js
@@ -36,6 +36,6 @@ featureLayer.on('ready', function() {
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('highway: ' + layer.feature.properties.highway);
+    layer.bindPopup('way: ' + layer.feature.properties.way);
   });
 });
