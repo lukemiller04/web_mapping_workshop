3263ee10369ec5ab3957bed8770bb02c5d86fdef
lukemiller04 <luke.miller@modot.mo.gov>
2015-02-19 15:09:25 -0600

    Update EDITEDapp_1.js

--- js/EDITEDapp_1.js
+++ js/EDITEDapp_1.js
@@ -16,7 +16,7 @@ var dataFileToAdd = 'data/Bike.geojson';
 var featureLayer = L.mapbox.featureLayer()
   
   featureLayer.loadurl(dataFileToAdd)
-  featureLayer.addTo(map)
+  featureLayer.addTo(map);
   
 featureLayer.on('ready' function(){
   this.setStyle({
