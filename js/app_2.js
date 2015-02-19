5057c6d68d497593d60be2eb95a2e5861958ca6e
lukemiller04 <luke.miller@modot.mo.gov>
2015-02-19 15:57:28 -0600

    Update app_2.js

--- js/app_2.js
+++ js/app_2.js
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/leveed_area.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/KCBike.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
